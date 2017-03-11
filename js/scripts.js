b2731022596e782ee7b2d628a7f439079952c823
zaramid <piotr@zawadzki.net>
2017-03-11 22:50:53 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var a = prompt("Podaj długość podstawy trójkąta (a)");
+var h = prompt("Podaj wysokość trójkąta (h)");
+var triangleArea = a * h / 2;
+console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea);
